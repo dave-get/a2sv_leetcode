d16c1407878f7af6ffdc65cd751af48820764ef2
Dawit Getachew <123627794+dave-get@users.noreply.github.com>
2023-12-25 08:05:47 -0900

    Added solution for Missing Number

--- leet-code-python-track/missing-number.py
+++ leet-code-python-track/missing-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        k={num for num in nums}
         for i in range(len(nums)+1):
-            if i in nums:
+            if i in k:
                 continue
             else:
-                if i+1 ==len(nums):
+                if i+1 ==len(k):
                     x=i
                     return x
                 else:
