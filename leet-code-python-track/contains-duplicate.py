04ba248b274d564a0efc466826e5e9f614fd8fde
Dawit Getachew <123627794+dave-get@users.noreply.github.com>
2023-12-28 07:20:45 -0900

    Delete leet-code-python-track/contains-duplicate.py
    Misplaced

--- leet-code-python-track/contains-duplicate.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        c = Counter(nums)
-        if any([val >= 2 for val in c.values()]):
-            return True
-        return False
