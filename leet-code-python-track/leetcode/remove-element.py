af70dc785f077369fb7092b1f762e7f419f22102
Dawit Getachew <123627794+dave-get@users.noreply.github.com>
2024-01-26 05:54:14 -0900

    Add solution for Remove Element

--- leet-code-python-track/leetcode/remove-element.py
+++ leet-code-python-track/leetcode/remove-element.py
@@ -1,16 +1,24 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        for i in range(len(nums)):
-            if nums[i] == val:
-                nums[i]="_"
-                count +=1
-            else:
-                nums[i] = str(nums[i])
-        nums.sort()
-        for i in range(len(nums)-count):
-            nums[i] = int(nums[i])
+        j=0
+        for v in nums:
+            if v!=val:
+                nums[j] = v
+                j+=1
+        return j
+        
+        
+        # count = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == val:
+        #         nums[i]="_"
+        #         count +=1
+        #     else:
+        #         nums[i] = str(nums[i])
+        # nums.sort()
+        # for i in range(len(nums)-count):
+        #     nums[i] = int(nums[i])
             
-        return len(nums) - count
+        # return len(nums) - count
         
         
