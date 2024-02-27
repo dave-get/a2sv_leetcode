a3087403d062039a87bfd2bdac00360f66ce8ba4
Dawit Getachew <123627794+dave-get@users.noreply.github.com>
2024-02-27 09:41:15 +0300

    Add solution for Daily Temperatures

---
+++ leet-code-python-track/leetcode/daily-temperatures.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        ans = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            while stack and stack[-1][0] < temperatures[i]:
+                val = stack.pop()
+                ans[val[1]] = i-val[1]         
+            stack.append((temperatures[i],i))
+        return ans
